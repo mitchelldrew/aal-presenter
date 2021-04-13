882a7265fb34947bac881b39fce601be533210a2
mitchdrew <mitchdrew@goatse.cx>
2021-04-13 15:11:16 -0700

    master - mdrew: cleaner irestprovider method signature

--- src/commonMain/kotlin/com/aal/presenter/IRestaurantProvider.kt
+++ src/commonMain/kotlin/com/aal/presenter/IRestaurantProvider.kt
@@ -3,13 +3,13 @@ package com.aal.presenter
 import com.aal.model.Restaurant
 
 interface IRestaurantProvider {
-    fun get(ne:LatLon, sw:LatLon)
+    fun get(neLat:Double, neLng:Double, swLat:Double, swLng:Double)
     fun search(query:String)
     fun addListener(restListener: Listener)
     fun removeListener(restListener: Listener)
 
     interface Listener {
-        fun onReceive(ne:LatLon, sw:LatLon, elements:List<Restaurant>)
+        fun onReceive(elements:List<Restaurant>)
         fun onReceive(query:String, elements:List<Restaurant>)
         fun onError(error:Exception)
     }
--- src/commonMain/kotlin/com/aal/presenter/LatLon.kt
+++
@@ -1,3 +0,0 @@
-package com.aal.presenter
-
-typealias LatLon = Pair<Double,Double>
